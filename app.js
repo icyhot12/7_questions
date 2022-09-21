f412085303a292052b54c5db28cbfae5ac34bfdb
icyhot12 <rmierniczek1@gmail.com>
2022-09-21 20:06:31 +0200

    added second method to logic

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const btns = document.querySelectorAll('.btn')
+// const btns = document.querySelectorAll('.btn')
 
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        const question = e.currentTarget.parentElement.parentElement;
+// btns.forEach(btn => {
+//     btn.addEventListener('click', e => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     })
+// })
+
+
+// secod method
+const questions = document.querySelectorAll('.question-container');
+
+questions.forEach(question => {
+    const btn = question.querySelector('.btn');
+
+    btn.addEventListener('click', () => {
         question.classList.toggle('show-text');
     })
 })
